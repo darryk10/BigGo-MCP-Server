1e0522922e6245cc804a2bc95121fccc56646a52
alex <alex@funmula.com>
2025-03-06 10:45:37 +0800

    fix: server not shutting down

--- src/biggo_mcp_server/__init__.py
+++ src/biggo_mcp_server/__init__.py
@@ -11,23 +11,9 @@ async def start():
 
     setting = BigGoMCPSetting()
     server = await create_server(setting)
-    server_bg: list[asyncio.Task] = []
-
-    logger.info("Starting STDIO BigGo MCP Server")
-    server_bg.append(
-        asyncio.create_task(server.run_stdio_async(), name="stdio-async"))
-
-    if setting.server_type == "sse":
-        logger.info("Starting SSE BigGo MCP Server")
-        server_bg.append(
-            asyncio.create_task(server.run_sse_async(), name="sse-async"))
-
-    await asyncio.gather(*server_bg)
+    await server.start()
+    await server.wait_finish()
 
 
 def main():
     asyncio.run(start())
-
-
-if __name__ == "__main__":
-    main()
